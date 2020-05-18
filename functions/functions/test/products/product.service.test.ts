cff9fa1737b42fb1ebdb9bab7bcf4a84e5df328c
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-05-18 21:49:50 +0200

    Update product.service.test.ts

--- functions/functions/test/products/product.service.test.ts
+++ functions/functions/test/products/product.service.test.ts
@@ -2,6 +2,7 @@ import {ProductService} from '../../src/products/product.service';
 import {StockRepository} from "../../src/stock/stock.repository";
 import {TestHelper} from "../helpers/test.helper";
 import {IMock} from 'moq.ts';
+import {Stock} from "../../src/models/stock";
 
 describe('ProductService', () => {
   // let productRepository: IMock<ProductRepository>;
@@ -18,6 +19,12 @@ describe('ProductService', () => {
     const product = testHelper.getProduct1();
     const stock = await productService.createStock(product);
     expect(stock).toBeDefined();
+  });
+
+  it('When making a new stock, its count should be bigger than zero', async () => {
+    const product = testHelper.getProduct1();
+    const stock: Stock = await productService.createStock(product);
+    expect(stock.count).toBeGreaterThan(0);
   });
 
 
